286bfd8a11a8473b396217a6c2cd66ef2e14da34
Roman Mettler <roman.mettler@gmail.com>
2025-05-05 02:49:48 -0300

    Add team participants list for tournament brackets

---
+++ src/data/brackets/teamList.ts
@@ -0,0 +1,67 @@
+export const TEAM_PARTICIPANTS_LIST = {
+    RAW_CHICKENS: {
+      id: "TEAM_1",
+      name: "Raw chickens",
+    },
+    GUERREROS_Z: {
+      id: "TEAM_2",
+      name: "Guerreros Z",
+    },
+    EFECTO_DIVINE: {
+      id: "TEAM_3",
+      name: "3fecto Divine",
+    },
+    PLAYERS_LIBRES: {
+      id: "TEAM_4",
+      name: "Players libres",
+    },
+    HYDRA_E_SPORTS: {
+      id: "TEAM_5",
+      name: "Hydra E-sports",
+    },
+    FANTASMAS: {
+      id: "TEAM_6",
+      name: "Fantasmas",
+    },
+    SUSHI_BOYS: {
+      id: "TEAM_7",
+      name: "Sushi boys",
+    },
+    LA_LAKERS: {
+      id: "TEAM_8",
+      name: "LA Lakers",
+    },
+    CHILDREN_OF_THE_CORN: {
+      id: "TEAM_9",
+      name: "Children of the corn",
+    },
+    WINDFURY_ENJOYERS: {
+      id: "TEAM_10",
+      name: "Windfury enjoyers",
+    },
+    ADELA_GAMING: {
+      id: "TEAM_11",
+      name: "Aldea Gaming",
+    },
+    THE_RATS: {
+      id: "TEAM_12",
+      name: "The rats",
+    },
+    BRANCA_TEAM: {
+      id: "TEAM_13",
+      name: "Branca Team",
+    },
+    TEAM_SERVER: {
+      id: "TEAM_14",
+      name: "Team server",
+    },
+    CARPINCHOS_RAGE: {
+      id: "TEAM_15",
+      name: "Carpinchos Rage",
+    },
+    LA_GATONETA: {
+      id: "TEAM_16",
+      name: "La Gatoneta",
+    },
+  };
+  

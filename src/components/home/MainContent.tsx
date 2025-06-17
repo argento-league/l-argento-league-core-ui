0468972e3d95d87377597dfce9d7af96c14de32e
Roman Mettler <roman.mettler@gmail.com>
2025-06-17 06:05:18 -0300

    feat: wrap MainContent in Container for improved layout structure

--- src/components/home/MainContent.tsx
+++ src/components/home/MainContent.tsx
@@ -4,6 +4,7 @@ import { MainCard } from "./cards/main";
 import { FeatureCard } from "./cards/features";
 import { NewsCard } from "./cards/news";
 import { SocialCard } from "./cards/socials";
+import { Container } from "../common/Container";
 
 const MainRootContainer = styled.div`
   width: 100%;
@@ -12,8 +13,9 @@ const MainRootContainer = styled.div`
   gap: 16px;
   background: #3b6377;
   background-image: url("/home-background.jpg");
-
   background-blend-mode: multiply;
+  background-size: cover;
+
   @media (max-width: 720px) {
     padding: 16px;
   }
@@ -28,7 +30,7 @@ const MainContentGroup = styled.div`
 
   @media (max-width: 1024px) {
     grid-template-columns: repeat(12, 1fr);
-    grid-template-rows: repeat(11, 150px);
+    grid-template-rows: repeat(10, 150px);
   }
 `;
 
@@ -61,93 +63,95 @@ const GridBox = styled.div<BoxProps>`
 export const MainContent = () => {
   return (
     <MainRootContainer>
-      <MainContentGroup>
-        <GridBox
-          col="1 / 13"
-          row="1 / 4"
-          colTablet="1 / 13"
-          rowTablet="1 / 4"
-          colMobile="1 / -1"
-          backgroundColor="#00000080"
-        >
-          <MainCard />
-        </GridBox>
-        <GridBox
-          col="13 / 21"
-          row="1 / 1"
-          colTablet="1 / 13"
-          rowTablet="6 / 8"
-          backgroundColor="transparent"
-        >
-          <SeasonWinner />
-        </GridBox>
-        <GridBox
-          col="13 / 21"
-          row="2 / 2"
-          colTablet="1 / 13"
-          rowTablet="11 / 12"
-        >
-          <SocialCard />
-        </GridBox>
-        <GridBox
-          col="1 / 7"
-          row="4 / 5"
-          colTablet="1 / 7"
-          rowTablet="4 / 5"
-          colMobile="1 / -1"
-        >
-          <FeatureCard
-            title={"🏆"}
-            description={"Liga tier 4 abierta \n para todos"}
-          />
-        </GridBox>
-        <GridBox
-          col="7 / 13"
-          row="4 / 5"
-          colTablet="7 / 13"
-          rowTablet="4 / 5"
-          colMobile="1 / -1"
-        >
-          <FeatureCard
-            title="🌎"
-            description={"Jugadores de toda \n Latinoamérica"}
-          />
-        </GridBox>
-        <GridBox
-          col="1 / 7"
-          row="5 / 6"
-          colTablet="1 / 7"
-          rowTablet="5 / 6"
-          colMobile="1 / -1"
-        >
-          <FeatureCard
-            title="📝"
-            description={
-              "Inscríbete con tu equipo o únete como jugador disponible"
-            }
-          />
-        </GridBox>
-        <GridBox
-          col="7 / 13"
-          row="5 / 6"
-          colTablet="7 / 13"
-          rowTablet="5 / 6"
-          colMobile="1 / -1"
-        >
-          <FeatureCard
-            title="⚔️"
-            description={"¡Demuestra tu nivel y \n sube en la escena!"}
-          />
-        </GridBox>
-        <GridBox
-          col="13 / 21"
-          row="3 / 6"
-          colTablet="1 / 13"
-          rowTablet="8 / 11"
-        >
-          <NewsCard />
-        </GridBox>
-      </MainContentGroup>
+      <Container>
+        <MainContentGroup>
+          <GridBox
+            col="1 / 13"
+            row="1 / 4"
+            colTablet="1 / 13"
+            rowTablet="1 / 3"
+            colMobile="1 / -1"
+            backgroundColor="#00000080"
+          >
+            <MainCard />
+          </GridBox>
+          <GridBox
+            col="13 / 21"
+            row="1 / 1"
+            colTablet="1 / 13"
+            rowTablet="5 / 7"
+            backgroundColor="transparent"
+          >
+            <SeasonWinner />
+          </GridBox>
+          <GridBox
+            col="13 / 21"
+            row="2 / 2"
+            colTablet="1 / 13"
+            rowTablet="10 / 11"
+          >
+            <SocialCard />
+          </GridBox>
+          <GridBox
+            col="1 / 7"
+            row="4 / 5"
+            colTablet="1 / 7"
+            rowTablet="3 / 4"
+            colMobile="1 / -1"
+          >
+            <FeatureCard
+              title={"🏆"}
+              description={"Liga tier 4 abierta \n para todos"}
+            />
+          </GridBox>
+          <GridBox
+            col="7 / 13"
+            row="4 / 5"
+            colTablet="7 / 13"
+            rowTablet="3 / 4"
+            colMobile="1 / -1"
+          >
+            <FeatureCard
+              title="🌎"
+              description={"Jugadores de toda \n Latinoamérica"}
+            />
+          </GridBox>
+          <GridBox
+            col="1 / 7"
+            row="5 / 6"
+            colTablet="1 / 7"
+            rowTablet="4 / 5"
+            colMobile="1 / -1"
+          >
+            <FeatureCard
+              title="📝"
+              description={
+                "Inscríbete con tu equipo o únete como jugador disponible"
+              }
+            />
+          </GridBox>
+          <GridBox
+            col="7 / 13"
+            row="5 / 6"
+            colTablet="7 / 13"
+            rowTablet="4 / 5"
+            colMobile="1 / -1"
+          >
+            <FeatureCard
+              title="⚔️"
+              description={"¡Demuestra tu nivel y \n sube en la escena!"}
+            />
+          </GridBox>
+          <GridBox
+            col="13 / 21"
+            row="3 / 6"
+            colTablet="1 / 13"
+            rowTablet="7 / 10"
+          >
+            <NewsCard />
+          </GridBox>
+        </MainContentGroup>
+      </Container>
     </MainRootContainer>
   );
 };

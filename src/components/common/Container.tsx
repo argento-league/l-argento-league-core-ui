50cddf1640840a7cb43d5c69020e959b9e79c51c
Roman Mettler <roman.mettler@gmail.com>
2025-06-17 06:02:55 -0300

    refactor: simplify Container component styles for better responsiveness

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,33 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  padding-left: 1rem;
-  padding-right: 1rem;
-
-  /* sm breakpoint - 640px */
-  @media (min-width: 640px) {
-    max-width: 640px;
-  }
-
-  /* md breakpoint - 768px */
-  @media (min-width: 768px) {
-    max-width: 768px;
-  }
-
-  /* lg breakpoint - 1024px */
-  @media (min-width: 1024px) {
-    max-width: 1024px;
-  }
+  margin: 0 auto;
+  width: 100%;
 
   /* xl breakpoint - 1280px */
   @media (min-width: 1280px) {
     max-width: 1280px;
-  }
-
-  /* 2xl breakpoint - 1536px */
-  @media (min-width: 1536px) {
-    max-width: 1536px;
   }
 `; 

6588d00d82036f27dd9fea342e6946734ff98b0a
Roman Mettler <roman.mettler@gmail.com>
2025-06-18 05:31:10 -0300

    feat: implement tabbed navigation and information display for Current Season page

--- src/pages/current-season/index.tsx
+++ src/pages/current-season/index.tsx
@@ -6,7 +6,7 @@ import styled from "styled-components";
 import { DateCard } from "../../components/current-season/cards/Dates";
 import { MainCard } from "../../components/current-season/cards/Main";
 import { LookingForTeamCard } from "../../components/current-season/cards/LookingForTeam";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 const MainContentGroup = styled.div`
   display: grid;
@@ -19,6 +19,83 @@ const MainContentGroup = styled.div`
     grid-template-columns: repeat(12, 1fr);
     grid-template-rows: repeat(7, 150px);
   }
+`;
+
+const TabsContainer = styled.div`
+  padding-top: 16px;
+  box-sizing: border-box;
+  color: white;
+  font-family: Outfit, sans-serif;
+  display: flex;
+`;
+
+const TabsWrapper = styled.div`
+  padding-left: 80px;
+  padding-right: 80px;
+  display: flex;
+  gap: 16px;
+  width: 100%;
+`;
+
+const InfoGeneralContainer = styled.div`
+  min-height: 300px;
+  padding: 64px;
+  gap: 34px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  color: white;
+`;
+
+const MenuContainer = styled.div`
+  max-width: 384px;
+  width: 100%;
+`;
+
+const ContentContainer = styled.div`
+  padding: 24px;
+  border: 1px solid #fabf4a;
+  border-radius: 16px;
+  font-family: Rethink Sans, sans-serif;
+  gap: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ContentTitle = styled.p`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const ContentText = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+const ContentListItem = styled.li`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+const NotImplementedContainer = styled.div`
+  min-height: 300px;
+  padding: 64px;
+  color: white;
+  font-family: Outfit, sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NotImplementedTitle = styled.h1`
+  font-size: 36px;
+  font-weight: 600;
+`;
+
+const NotImplementedText = styled.p`
+  font-size: 18px;
+  font-weight: 400;
 `;
 
 type GridElementProps = {
@@ -82,7 +159,7 @@ const bentoElements: GridElementProps[] = [
       backgroundColor: "inherit",
     },
     component: <DateCard />,
-  }
+  },
 ];
 
 export const CurrentSeasonPage = () => {
@@ -91,20 +168,193 @@ export const CurrentSeasonPage = () => {
       backgroundImage="/current-season-background.jpg"
       backgroundBlendMode="lighten"
       background="#000000"
-      backgroundPosition="bottom"
+      backgroundPosition="center"
     >
       <Container>
         <MainContentGroup>
           {bentoElements.map((element) => (
-            <GridBox
-              key={element.card}
-              {...element.gridProps}
-            >
+            <GridBox key={element.card} {...element.gridProps}>
               {element.component}
             </GridBox>
           ))}
         </MainContentGroup>
+        <CurrentSeasonDetails />
       </Container>
     </MainRootContainer>
+  );
+};
+
+type TabsProps = {
+  selected?: boolean;
+};
+
+const Tabs = styled.div<TabsProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  padding: 16px 32px;
+  width: 100%;
+  text-align: center;
+  font-family: "Outfit", sans-serif;
+  font-weight: ${(props) => (props.selected ? "600" : "300")};
+  ${(props) => (props.selected ? "border-bottom: 1px solid #fabf4a;" : "")};
+`;
+
+enum TabsEnum {
+  InfoGeneral = "Info General",
+  Equipos = "Equipos",
+  FaseDeGrupos = "Fase de grupos",
+  EventoPrincipal = "Evento Principal",
+}
+
+const CurrentSeasonDetails = () => {
+  const [selectedTab, setSelectedTab] = useState<TabsEnum>(
+    TabsEnum.InfoGeneral
+  );
+  return (
+    <div>
+      <TabsContainer>
+        <TabsWrapper>
+          {Object.values(TabsEnum).map((tab) => (
+            <Tabs
+              key={tab}
+              selected={selectedTab === tab}
+              onClick={() => setSelectedTab(tab as TabsEnum)}
+            >
+              {tab}
+            </Tabs>
+          ))}
+        </TabsWrapper>
+      </TabsContainer>
+      {selectedTab === TabsEnum.InfoGeneral ? (
+        <InfoGeneral />
+      ) : (
+        <NotImplemented />
+      )}
+    </div>
+  );
+};
+
+type MenuItemContainerProps = {
+  isSelected: boolean;
+};
+
+const MenuItemContainer = styled.div<MenuItemContainerProps>`
+  padding: 16px;
+  background-color: ${(props) => (props.isSelected ? "#FABF4A33" : "inherit")};
+  border-radius: 8px;
+  cursor: pointer;
+  font-family: Outfit, sans-serif;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const InfoGeneral = () => {
+  const [selectedSubject, setSelectedSubject] = useState<INFORMATION_ENUM>(
+    INFORMATION_ENUM.ARMADO_DE_EQUIPO
+  );
+
+  return (
+    <InfoGeneralContainer>
+      <MenuContainer>
+        <MenuItemContainer
+          isSelected={selectedSubject === INFORMATION_ENUM.ARMADO_DE_EQUIPO}
+          onClick={() => setSelectedSubject(INFORMATION_ENUM.ARMADO_DE_EQUIPO)}
+        >
+          <span>Armado de equipos</span>
+        </MenuItemContainer>
+        <MenuItemContainer
+          isSelected={selectedSubject === INFORMATION_ENUM.CAPITANES}
+          onClick={() => setSelectedSubject(INFORMATION_ENUM.CAPITANES)}
+        >
+          <span>Capitanes</span>
+        </MenuItemContainer>
+        <MenuItemContainer
+          isSelected={selectedSubject === INFORMATION_ENUM.CUENTAS_DE_STEAM}
+          onClick={() => setSelectedSubject(INFORMATION_ENUM.CUENTAS_DE_STEAM)}
+        >
+          <span>Cuentas de Steam</span>
+        </MenuItemContainer>
+        <MenuItemContainer
+          isSelected={selectedSubject === INFORMATION_ENUM.FECHAS_DE_JUEGO}
+          onClick={() => setSelectedSubject(INFORMATION_ENUM.FECHAS_DE_JUEGO)}
+        >
+          <span>Fechas de juego</span>
+        </MenuItemContainer>
+      </MenuContainer>
+      <div style={{width: "100%"}}>
+        <ContentContainer>
+          <ContentTitle>{information[selectedSubject].title}</ContentTitle>
+          {information[selectedSubject].description?.length > 1 ? (
+            information[selectedSubject].description.map((el) => (
+              <ContentListItem key={el}>{el}</ContentListItem>
+            ))
+          ) : (
+            <ContentText>
+              {information[selectedSubject].description}
+            </ContentText>
+          )}
+        </ContentContainer>
+      </div>
+    </InfoGeneralContainer>
+  );
+};
+
+type Information = {
+  subject?: string;
+  title: string;
+  description: string[];
+};
+
+enum INFORMATION_ENUM {
+  ARMADO_DE_EQUIPO = "Armado de equipos",
+  CAPITANES = "Capitanes",
+  CUENTAS_DE_STEAM = "Cuentas de Steam",
+  FECHAS_DE_JUEGO = "Fechas de juego",
+}
+
+const information: Record<INFORMATION_ENUM, Information> = {
+  [INFORMATION_ENUM.ARMADO_DE_EQUIPO]: {
+    title: "Medalla libre",
+    description: [
+      "La medalla de los jugadores es libre. Los equipos pueden conformarse con jugadores de cualquier medalla, el único requisito es que el equipo en juego no debe superar la suma de 36.500 de mmr.",
+      "El roster puede contar con 5 titulares y 3 suplentes.",
+      "Esta limitado los jugadores peruanos nuevos por equipos a 2.",
+    ],
+  },
+  [INFORMATION_ENUM.CAPITANES]: {
+    title: "Capitanes",
+    description: [
+      "Todos los capitanes de un equipo deben ser hispanohablantes y comunicarse con el staff para poder ser notificados de los eventos de la season.",
+    ],
+  },
+  [INFORMATION_ENUM.CUENTAS_DE_STEAM]: {
+    title: "Players en la liga",
+    description: [
+      "Todas las cuentas tienen que estar calibradas, tener mas de 5k horas y 4k de partidas (cualquier duda consultar con el Staff).",
+      "Perfil de dota y steam tienen que ser públicos al momento de la inscripción.",
+    ],
+  },
+  [INFORMATION_ENUM.FECHAS_DE_JUEGO]: {
+    title: "Días y horarios",
+    description: [
+      "El staff definirá las semanas en las que se deba jugar cada instancia de la season.",
+      "Los días y horarios serán arreglados por los capitanes de los equipos y notificados al staff.",
+      "En caso de no acordar un día y horario entre capitanes, el staff definirá uno por defecto.",
+    ],
+  },
+};
+
+const NotImplemented = () => {
+  return (
+    <NotImplementedContainer>
+      <NotImplementedTitle>
+        Esta sección aún no está implementada
+      </NotImplementedTitle>
+      <NotImplementedText>
+        Estamos trabajando para traerte la mejor experiencia posible.
+      </NotImplementedText>
+    </NotImplementedContainer>
   );
 };
